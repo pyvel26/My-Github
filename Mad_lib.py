ab0e3e13d73ca5f91ec923db52dc5101baa4e319
velben27 <bennettestervelle@gmail.com>
2021-03-28 17:57:29 -0400

    Created my second project

---
+++ Mad_lib.py
@@ -0,0 +1,11 @@
+
+animal = input("Enter a animal: ")
+sport = input("Enter a sport: ")
+body_part = input("Enter a body part: ")
+favorite_cookie = input("Enter your favorite_cookie: ")
+
+print("My boss is a " + animal)
+print("When bored I scream " +  sport)
+print("I can't feel my " + body_part)
+print("My hair is filled with " + favorite_cookie)
+
